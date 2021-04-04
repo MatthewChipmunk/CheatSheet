f00e9c81fc6a93388549fd07a7646cc2af2a19ce
MatthewChipmunk <81892006+MatthewChipmunk@users.noreply.github.com>
2021-04-04 08:42:47 -0400

    Update Hackathon.py

--- Hackathon.py
+++ Hackathon.py
@@ -1,9 +1,9 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter.font import Font
-import math
 
 
+#
 # import numpy
 # import cv2
 
@@ -24,14 +24,14 @@ def Midpoint():
 def pt():
     messagebox.showinfo(
         'Pythagorean Theorem',
-        'The Pythagorean Theorem is a^2 + b^2 = c^2 Where a is any leg, b is the other leg, and c is the hypotenuse '
+        'The Pythagorean Theorem is a² + b² = c² Where a is any leg, b is the other leg, and c is the hypotenuse '
     )
 
 
 def am():
     messagebox.showinfo(
         'Arithmetic Mean',
-        'The Arithmetic mean of the n numbers x_1,x_2,...,x_n is (x₁+x₂+...+x_n-1, x_n)'
+        'The Arithmetic mean of the n numbers x₁,x₂,...,x_n is (x₁+x₂+...+x_n-1, x_n)'
     )
 
 
@@ -45,28 +45,34 @@ def gm():
 def rightTriangles1():
     messagebox.showinfo(
         'Right Triangles',
-        'If the angles of a right triangle are 45, 45, and 90, then the length of the two legs are the same, and the hypotenuse is x * sqrt2. If the angles of a right triangle are 30, 60, and 90, then the longer leg is sqrt3 * the shorter leg, and the hypotenuse is 2 times the shorter leg. '
+        'If the angles of a right triangle are 45, 45, and 90, then the length of the two legs are the same, '
+        'and the hypotenuse is x * sqrt2. If the angles of a right triangle are 30, 60, and 90, then the longer leg '
+        'is sqrt3 * the shorter leg, and the hypotenuse is 2 times the shorter leg. '
     )
 
 
 def LinesIncircles():
     messagebox.showinfo(
         'Lines in Circle',
-        'The diameter of a circle is the longest chord in the circle. A chord is any line in a circle that connects 2 points on the circle. The radius is the line segment from the center of the circle to the circumfrence of a circle. The radius is the half the diameter. None of these lines can be less than or equal to 0'
+        'The diameter of a circle is the longest chord in the circle. A chord is any line in a circle that connects 2 '
+        'points on the circle. The radius is the line segment from the center of the circle to the circumference of a '
+        'circle. The radius is the half the diameter. None of these lines can be less than or equal to 0 '
     )
 
 
 def cc():
     messagebox.showinfo(
         'Circumference of Circle',
-        '''The Circumference of a circle is πd, where π is pi, which is approximately 3.14159265358979323. d stands for diameter, which is not 0'''
+        '''The Circumference of a circle is πd, where π is pi, which is approximately 3.14159265358979323. d stands 
+        for diameter, which is not 0 '''
     )
 
 
 def ac():
     messagebox.showinfo(
         'Area of touches',
-        '''The area of a circle is π*(r^2), where π is pi(approximately 3.14159265358979323). r stands for radius, which is half the diameter '''
+        'The area of a circle is π*(r^2), where π is pi(approximately 3.14159265358979323). r stands for radius, '
+        'which is half the diameter '
     )
 
 
@@ -87,14 +93,14 @@ def modulo():
 def basic_probability():
     messagebox.showinfo(
         'Basic Probability',
-        '''The formula for probability is (wanted outcomes)/(total outcomes)'''
+        '''The formula for probability is \n(wanted outcomes)/(total outcomes)'''
     )
 
 
 def casework():
     messagebox.showinfo(
         'Casework', '''Casework is when you manually 
-    count all the possibes cases''')
+    count all the possible cases''')
 
 
 def complementary():
@@ -106,12 +112,51 @@ def complementary():
 
 def factors():
     messagebox.showinfo('Factors',
-                        '''for a number with the prime factorization p1^e1*p2^e2*...pk*ek the number of factors is (e1+1)*(e2+1)*...(en+1)''')
+                        'for a number with the prime factorization p1^e1*p2^e2*...pk*ek the number of factors is'
+                        '(e1+1)*(e2+1)*...(en+1)')
 
 
 def Constructive():
     messagebox.showinfo('Constructive Counting',
-                        'Constructive counting is counting the number of integers, lists, etc., that satisfy a certain property by "constructing" it. This strategy is useful for complicated counting or combinatorics questions, as the problem can be broken down into cases, the number of possibilities in each case can be computed, and those numbers can be summed to find the final answer.')
+                        'Constructive counting is counting the number of integers, lists, etc., that satisfy a '
+                        'certain property by "constructing" it. This strategy is useful for complicated counting or '
+                        'combinatorics questions, as the problem can be broken down into cases, the number of '
+                        'possibilities in each case can be computed, and those numbers can be summed to find the '
+                        'final answer.')
+
+
+def dis():
+    messagebox.showinfo('The Distance, Speed and Time Formulas', 'The formula for distance,D is the distance, is D=RT,'
+                                                                 'when R, the rate, is any number that\'s positive '
+                                                                 'and '
+                                                                 'T,the time, is any number that\'s also positive.\n'
+                                                                 '_______________________________________________'
+                                                                 'The formula for speed, or rate, is R = D/T, R is '
+                                                                 'the rate, when D, the distance, is any number '
+                                                                 'that\'s positive and T, the time, is any number the '
+                                                                 'also positive\n'
+                                                                 '_______________________________________________'
+                                                                 'The formula for time, is T = D/R, T is time, when '
+                                                                 'D, the Distance, is any number that\'s positive and '
+                                                                 'R, the rate or speed, is also any number that\'s '
+                                                                 'positive')
+
+
+def q():
+    messagebox.showinfo('Quadratic Formula', 'To solve any quadratic in the form ax²+bx+c = 0, where a is not 0, '
+                                             'use this:\n(-b±sqrt(b²-4ac))/(2a)')
+
+
+def f():
+    messagebox.showinfo('Use Factorizations', 'Here are some useful factorizations:\n x²-y² = (x-y)(x+y)\n'
+                                              'x²-2xy+y²=(x-y)²\nx²+2xy+y²=(x+y)²\nx^3-y^3=(x-y)(x²+xy+y²)\n'
+                                              'x^3+y^3=(x+y)(x²-xy+y²)\nax+ay+bx+xy=(a+x)(b+y)')
+
+
+def v():
+    messagebox.showinfo('Vieta\'s formula', 'Vieta\'s formula states that for any quadratic in the form ax²+bx+x=0, '
+                                            'where a≠0, then\n'
+                                            'Sum of roots=-b/a\nProduct of roots=c/a')
 
 
 # Main Page
@@ -146,6 +191,10 @@ Button(algeria, text='Slope Formula', command=Slope, height=0).pack()
 Button(algeria, text='Midpoint Formula', command=Midpoint, height=0).pack()
 Button(algeria, text='Arithmetic Mean', command=am, height=0).pack()
 Button(algeria, text='Geometry Mean', command=gm, height=0).pack()
+Button(algeria, text='Distance, Speed, and Time formula', height=0, command=dis).pack()
+Button(algeria, text='Quadratic Formula', height=0, command=q).pack()
+Button(algeria, text='Useful Factorizations', height=0, command=f).pack()
+Button(algeria, text='Vieta\'s Formula', height=0, command=v).pack()
 # Number Theory
 nt = LabelFrame(root, text='Number Theory', bg='orange', fg="blue")
 nt.pack(padx=5, pady=5)
